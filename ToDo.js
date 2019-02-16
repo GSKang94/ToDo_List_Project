c502f5a3dcd294b74926751a41cb3db102482427
Abdullah <abdullah.akcam52@gmail.com>
2019-02-16 16:07:09 +0300

    Added button with basic functionality to add items

--- ToDo.js
+++ ToDo.js
@@ -1,1 +1,11 @@
+// ToDo.js
 
+// Add task to list
+function addTask() {
+    var li = document.createElement("LI");
+    var input = document.getElementById("task-name");
+    li.innerHTML = input.value;
+
+    document.getElementById("todo-list").appendChild(li);
+    // console.log("Test addTask() function");
+}
