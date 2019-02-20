af41c22e59b4077c6e02754b3449a6363faf20f3
Gagandeep <singh.gagandeep0034@gmail.com>
2019-02-19 19:23:07 -0600

    My Version

--- ToDo.js
+++ ToDo.js
@@ -1,19 +1,17 @@
-// ToDo.js
-
-// Add task to list
-function addTask() {
-    var li = document.createElement("LI");
-    var input = document.getElementById("task-name");
-    li.innerHTML = input.value;
-
-    document.getElementById("todo-list").appendChild(li);
-    // console.log("Test addTask() function");
-}
-
-
 // Toggle Item as done after clicking.
-var ul = document.querySelector("ul");
 
+var li = document.querySelector("li");
+var ul = document.querySelector("ul");
+var input = document.querySelector("input[type='text']");
+
+/* Without dynamic elements
+for (var i = 0; i < items.length; i++) {
+	items[i].addEventListener("click", function(){
+		this.classList.toggle("tDisplay")
+	})
+}*/
+
+// With dynamic elements
 ul.addEventListener("click", setCompleted);
 
 function setCompleted(e) {
@@ -21,3 +19,41 @@ function setCompleted(e) {
 		e.target.classList.toggle("completed");
 	}
 }
+
+
+// remove the todo after hitting X.
+
+// With dynamic elements.
+ul.addEventListener("click", setDeleted);
+
+function setDeleted(e) {
+	if (e.target.tagName.toLowerCase() === 'span') {
+		e.target.parentNode.remove();
+		e.stopPropagation(); 
+	}
+}
+
+/* without dynamic elements.
+var spans = document.querySelectorAll("span");
+
+for (var i = 0; i < spans.length; i++) {
+	spans[i].addEventListener("click", function(){
+		this.parentNode.remove()
+	})
+}*/
+
+// add new todo to the list upon hitting enter key.
+
+input.addEventListener("keydown", function(){
+	if (event.keyCode === 13) {
+		var val = this.value;
+		newItem = document.createElement("li");
+		newItem.innerHTML = "<span> X </span>" + val;
+		ul.appendChild(newItem)
+		input.value = "";
+	}
+})
+
+// add effects.
+// add icons.
+// style the app.
